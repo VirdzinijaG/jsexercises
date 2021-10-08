e16f9e1278130907d84e0e59f3d5817a89804500
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-08 13:53:35 +0300

    ...

--- masyvai.js
+++ masyvai.js
@@ -103,3 +103,62 @@ for (let i = arr.length; i < 30; i++) {
 console.log(arr, "Masyvo ilgis ", arr.length);
 
 console.log("-------------------");
+
+// 8 Naudojant 1 uždavinio masyvą iš jo reikšmių sukurti dar du papildomus masyvus. Į vieną iš 1 uždavinio masyvo pridėti reikšmes mažesnes arba lygias 2 (monetas), o į kitą didesnes nei 2 (popierinius pinigus);
+
+console.log("-----", 8, '------');
+
+console.log(arr);
+
+let arr1 = [];
+let arr2 = [];
+
+for (let i in arr) {
+    if (arr[i] <= 2) {
+        arr1.push(arr[i])
+    } else {
+        arr2.push(arr[i])
+    }
+}
+
+console.log(arr1);
+console.log(arr2);
+
+console.log("-------------------");
+
+// 9 Sukurti masyvą (piniginę su dviem skyreliais) iš dviejų elementų, kurio pirmas elementas būtų masyvas iš 8 uždavinio su monetom, o antras elementas masyvas iš 8 uždavinio su popieriniais pinigais;
+
+console.log("-----", 9, '------');
+
+let skyrelis = [];
+
+skyrelis.push(arr1);
+skyrelis.push(arr2);
+
+
+console.log(skyrelis);
+
+console.log("-------------------");
+
+// 10 Į 9 uždavinio masyvą, piniginę su dviem skyreliais, pridėti trečią skyrelį- masyvą su kortelėm: ['KIKA', 'Euro Vaistinė', 'Drogas', 'Ačiū', 'Lietuvos Geležinkeliai', 'Mano RIMI'];
+
+console.log("-----", 10, '------');
+
+let korteles = ['KIKA', 'Euro Vaistinė', 'Drogas', 'Ačiū', 'Lietuvos Geležinkeliai', 'Mano RIMI'];
+
+skyrelis.push(korteles);
+
+console.log(skyrelis);
+
+console.log("-------------------");
+
+// 11 Korteles skyrelyje sudėlioti (išrūšiuoti) pagal abėcėlę;
+
+console.log("-----", 11, '------');
+
+skyrelis[2].sort();
+console.log(skyrelis);
+
+console.log("-------------------");
+
+// 12 Į kortelių skyrelį pridėti mokėjimo kortelių 'MasterCard', 'Visa' (su rand generuokite atsitiktines reikšmes 'MasterCard' arba 'Visa' ir rašykite į masyvą) iš skirtingų bankų tiek, kad skyrelis (masyvo ilgis) pasidarytų lygus 20;
